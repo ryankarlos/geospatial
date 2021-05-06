e8896d50808318e69c2e8f2de8063bef1628f672
Ryan Nazareth <ryankarlos@gmail.com>
2021-05-06 23:13:20 +0100

    refactor setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="Geospatial Analysis in Python",
-    version="0.1",
-    description="``geospatial librairy tutorials",
-    author="Ryan Nazareth",
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-)
+
+# Get the long description from the README file
+with open('README.md') as f:
+    long_description = f.read()
+
+if __name__ == "__main__":
+    setup(
+        name="Geospatial Analysis in Python",
+        version="0.1",
+        description="``geospatial librairy tutorials",
+        long_description=long_description,
+        author="Ryan Nazareth",
+        packages=find_packages(exclude=["tests", "data/ESRI"]),
+        project_urls={'Source': 'https://github.com/ryankarlos/Geospatial'},
+        python_requires='>=3.9',
+    )
